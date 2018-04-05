c583abeb66bee7b8aa9164aa78011a5a025e84d6
Dmitriy <vireas@hotmail.com>
2018-04-05 18:30:22 +0400

    Update urls.py

--- urls.py
+++ urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('apps.core.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
